559148880a1eb867322379a31038243403430729
EverWinter23 <eternal.blizzard23@gmail.com>
2019-09-09 00:50:51 +0530

    feat: GEOS Geometry converter to Graphene scalars

---
+++ graphene_gis/converter.py
@@ -0,0 +1,17 @@
+from django.contrib.gis.db import models as gis_models
+from graphene_django.converter import convert_django_field
+from graphene_gis import scalars
+
+GIS_FIELD_SCALAR = {
+    "PointField": scalars.PointScalar,
+    "LineStringField": scalars.LineStringScalar,
+}
+
+
+@convert_django_field.register(gis_models.LineStringField)
+@convert_django_field.register(gis_models.PointField)
+def gis_converter(field, registry=None):
+    class_name = field.__class__.__name__
+    return GIS_FIELD_SCALAR[class_name](
+        required=not field.null, description=field.help_text
+    )
---
+++ graphene_gis/scalars.py
@@ -0,0 +1,44 @@
+from graphql.language import ast
+from graphene.types import Scalar
+from django.contrib.gis.geos import GEOSGeometry
+import pdb
+
+
+class GISScalar(Scalar):
+    @property
+    def geom_typeid(self):
+        raise NotImplementedError(
+            "GEOSScalar is an abstract class and doesn't have a 'geom_typeid'. \
+            Instantiate a concrete subtype instead."
+        )
+
+    @staticmethod
+    def serialize(geometry):
+        return eval(geometry.geojson)
+
+    @classmethod
+    def parse_literal(cls, node):
+        # pdb.set_trace()
+        assert isinstance(node, ast.StringValue)
+        geometry = GEOSGeometry(node.value)
+        return eval(geometry.geojson)
+
+    @classmethod
+    def parse_value(cls, node):
+        pdb.set_trace()
+        geometry = GEOSGeometry(node.value)
+        return eval(geometry.geojson)
+
+
+class PointScalar(GISScalar):
+    geom_typeid = 0
+
+    class Meta:
+        description = "A GIS Point geojson"
+
+
+class LineStringScalar(GISScalar):
+    geom_typeid = 1
+
+    class Meta:
+        description = "A GIS LineString geojson"
